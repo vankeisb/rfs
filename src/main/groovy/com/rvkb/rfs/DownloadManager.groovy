89be3fbd44756b8074b6966075ab8e493b35d9ad
Remi Vankeisbelck <remi@rvkb.com>
2012-03-02 18:28:55 +0100

    broadcaster create/update/delete

--- src/main/groovy/com/rvkb/rfs/DownloadManager.groovy
+++ src/main/groovy/com/rvkb/rfs/DownloadManager.groovy
@@ -8,6 +8,8 @@ import org.apache.http.HttpResponse
 import org.apache.http.client.ResponseHandler
 import woko.hibernate.TxCallbackWithResult
 import com.rvkb.rfs.model.Config
+import woko.hibernate.TxCallback
+import com.rvkb.rfs.model.Download
 
 class DownloadManager {
 
@@ -33,6 +35,11 @@ class DownloadManager {
             String targetFile = "$baseDir$relativePath"
 
             println "*** Downloading : $url to $targetFile"
+
+            store.doInTx({ st, session ->
+                Download d = new Download(buddy: buddy, relativePath: relativePath)
+                store.save(d)
+            } as TxCallback)
 
             HttpGet get = new HttpGet(url)
             httpclient.execute(get, { HttpResponse r ->
---
+++ src/main/groovy/com/rvkb/rfs/model/Download.groovy
@@ -0,0 +1,24 @@
+package com.rvkb.rfs.model
+
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.GeneratedValue
+import javax.validation.constraints.NotNull
+import javax.persistence.ManyToOne
+import javax.persistence.FetchType
+
+@Entity
+class Download {
+
+    @Id
+    @GeneratedValue
+    Long id
+
+    @NotNull
+    String relativePath
+
+    @NotNull
+    @ManyToOne(fetch=FetchType.LAZY)
+    User buddy
+
+}
